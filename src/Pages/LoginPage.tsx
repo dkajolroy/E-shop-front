3fd607d785187c2ba16e22ceaf68daff130be5c6
kajolroy <shrikajol@gmail.com>
2023-01-20 12:54:23 +0600

    login registration design

--- src/Pages/LoginPage.tsx
+++ src/Pages/LoginPage.tsx
@@ -1,5 +1,12 @@
 import React from "react";
 
 export default function LoginPage() {
-  return <div>LoginPage</div>;
+  return <div className='col-md-3 col-11 m-auto'>
+    <div className="rounded shadow p-3">
+      <h3 className='text-center'>Login</h3>
+      <input className='form-control my-3' type="text" placeholder="Username" />
+      <input className='form-control my-3' type="text" placeholder="Password" />
+      <input className='btn btn-success w-100' type="submit" value="Login"/>
+    </div>
+  </div>;
 }
--- src/Pages/RegisterPage.tsx
+++ src/Pages/RegisterPage.tsx
@@ -1,5 +1,13 @@
 import React from "react";
 
 export default function RegisterPage() {
-  return <div>RegisterPage</div>;
+  return <div className='col-md-3 col-11 m-auto'>
+    <div className="rounded shadow p-3">
+      <h3 className='text-center'>Registration</h3>
+      <input className='form-control my-3' type="text" placeholder="Username" />
+      <input className='form-control my-3' type="text" placeholder="Password" />
+      <input className='form-control my-3' type="text" placeholder="Re-type password" />
+      <input className='btn btn-success w-100' type="submit" value="Login"/>
+    </div>
+  </div>;
 }
